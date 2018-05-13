356b2ff95979cde2fa680e6d01f59662daeef08b
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-05-14 00:13:02 +0530

    Material for React in place

--- app/javascript/components/SampleComponent.js
+++ app/javascript/components/SampleComponent.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn,} from 'material-ui/Table';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class SampleComponent extends React.Component{
     render(){
         return(
-            <h1>Hi, I'm Sample Component!</h1>
+            <MuiThemeProvider>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHeaderColumn>ID</TableHeaderColumn>
+                            <TableHeaderColumn>Name</TableHeaderColumn>
+                            <TableHeaderColumn>Status</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableRowColumn>1</TableRowColumn>
+                            <TableRowColumn>John Smith</TableRowColumn>
+                            <TableRowColumn>Employed</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>2</TableRowColumn>
+                            <TableRowColumn>Randal White</TableRowColumn>
+                            <TableRowColumn>Unemployed</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>3</TableRowColumn>
+                            <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                            <TableRowColumn>Employed</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>4</TableRowColumn>
+                            <TableRowColumn>Steve Brown</TableRowColumn>
+                            <TableRowColumn>Employed</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>5</TableRowColumn>
+                            <TableRowColumn>Christopher Nolan</TableRowColumn>
+                            <TableRowColumn>Unemployed</TableRowColumn>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </MuiThemeProvider>
         );
     }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import SampleComponent from '../components/SampleComponent';
+import './application.css'
+import "bootstrap/dist/css/bootstrap.css";
 
 document.addEventListener('DOMContentLoaded', () => {
    const container = document.body.appendChild(document.createElement('div'));
